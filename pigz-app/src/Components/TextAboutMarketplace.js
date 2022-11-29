69325dab9ea9e6bf378837496d525b07d9d31a77
thais oliveira <atlasoliveira2001@gmail.com>
2022-11-29 17:31:46 -0400

    feat: implements text areas about marketplace and support

---
+++ pigz-app/src/Components/TextAboutMarketplace.js
@@ -0,0 +1,10 @@
+function TextAboutMarketplace() {
+    return (
+      <div>
+        <p>Além de uma página exclusiva, com o Pigz Marketplace sua loja terá mais visibilidade, no app e no site. Uma vitrine com milhares de clientes todos os dias, pra vender muito mais.</p>
+      </div>
+    );
+  }
+  
+  export default TextAboutMarketplace;
+  
---
+++ pigz-app/src/Components/TextAboutSupportCard.js
@@ -0,0 +1,10 @@
+function TextAboutSupportCard() {
+    return (
+        <div>
+            <h1>E mais: suporte que realmente funciona!</h1>
+            <p>Respostas automáticas e robôs? Aqui não. Nossa equipe está sempre disponível pra ajudar você e seus clientes.</p>
+        </div>
+    );
+  }
+  
+  export default TextAboutSupportCard;
