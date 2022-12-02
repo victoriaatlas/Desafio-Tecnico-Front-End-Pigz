d8d8c0e1ad47bcd2b2311309a826f050c611c46e
thais oliveira <atlasoliveira2001@gmail.com>
2022-12-02 02:16:15 -0400

    feat: implements new components, adds functionality that listens for button clicks

--- pigz-app/src/Styles/SupportContent.js
+++ pigz-app/src/Styles/SupportContent.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const DivText = styled.div`
   background-image: linear-gradient(to bottom, #FA641E, #FF881F);
-  width: 90%;
+  width: 95%;
   color: #FFFFFF;
   border-radius: 43px;
   display: flex;
@@ -19,3 +19,29 @@ export const Img = styled.img`
 export const P = styled.p`
   padding: 30px`
 ;
+
+export const Button = styled.button`
+  border: 1px #FFFFFF;
+  background-color: #FFFFFF;
+`
+
+export const H1 = styled.h1`
+  font-size: 25px;
+  padding: 10px;
+  color: #333333;
+`
+
+export const Section = styled.section`
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`
+
+ export const Input = styled.input`
+  height: 5px;
+  border: 1px #FFFFFF;
+  background-color: #EEEEEE;
+  width: 80px;
+  border-radius: 20px;
+`
